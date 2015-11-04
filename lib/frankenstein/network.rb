985cf2fc96fba4a9495d5a4b1d8f1bb6f18db3b9
dkhamsing <dkhamsing@cirruspath.com>
2015-11-04 08:42:15 -0800

    cleanup

--- lib/frankenstein/network.rb
+++ lib/frankenstein/network.rb
@@ -12,6 +12,17 @@ module Frankenstein
       Faraday.get(url)
     end
 
+    def net_find_github_url_readme(repo, branch)
+      base = "#{GITHUB_RAW_CONTENT_URL}#{repo}/#{branch}/"
+      readme = README_VARIATIONS.find do |x|
+        temp = "#{base}#{x}"
+        net_status(temp) == 200
+      end
+      url = "#{base}#{readme}"
+
+      [url, readme]
+    end
+
     def net_status(url)
       response = net_head(url)
       code = response.status
@@ -37,17 +48,6 @@ module Frankenstein
     rescue Faraday::Error, Faraday::Error::ConnectionFailed => e
       log.verbose "fetch_response error #{e}"
       return nil
-    end
-
-    def net_find_github_url_readme(repo, branch)
-      base = "#{GITHUB_RAW_CONTENT_URL}#{repo}/#{branch}/"
-      readme = README_VARIATIONS.find do |x|
-        temp = "#{base}#{x}"
-        net_status(temp) == 200
-      end
-      url = "#{base}#{readme}"
-
-      [url, readme]
     end
   end # class
 end
