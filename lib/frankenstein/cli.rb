50de232ae54f8ccf411d5880bd1ecdc7561342b4
dkhamsing <dkhamsing@cirruspath.com>
2015-11-06 09:12:59 -0800

    cleanup cli process

--- lib/frankenstein/cli.rb
+++ lib/frankenstein/cli.rb
@@ -35,35 +35,76 @@ module Frankenstein
       argv_flags.to_s.include? FLAG_VERBOSE
     end
 
-    def cli_process(argv1, argv_flags, log)
-      option_github_stars_only = ARGV.include? OPTION_STARS
-      option_head = ARGV.include? OPTION_HEAD
-      flag_control_failure = argv_flags.to_s.include? FLAG_FAIL
-
-      if argv1
-        argv1_is_http = argv1.match(/^http/)
-
-        unless argv1_is_http
-          begin
-            found_file_content = File.read(argv1)
-          rescue StandardError => e
-            log.verbose "Not a file: #{e.to_s.red}"
-          end
-
-          option_pull_request = if argv1.include? '/'
-                                  ARGV.include? OPTION_PULL_REQUEST
-                                else
-                                  false
-                                end
-        end
-      end
-
+    def cli_process_flags(option_github_stars_only, argv_flags)
       flag_fetch_github_stars = cli_get_github(option_github_stars_only,
                                                argv_flags)
       flag_minimize_output = argv_flags.to_s.include? FLAG_MINIMIZE_OUTPUT
+      flag_control_failure = argv_flags.to_s.include? FLAG_FAIL
+
+      [
+        flag_fetch_github_stars,
+        flag_minimize_output,
+        flag_control_failure
+      ]
+    end
+
+    def cli_process_options(argv1, argv1_is_http, log)
+      option_github_stars_only = ARGV.include? OPTION_STARS
+      option_head = ARGV.include? OPTION_HEAD
+
+      unless argv1_is_http
+        option_pull_request = if argv1.include? '/'
+                                ARGV.include? OPTION_PULL_REQUEST
+                              else
+                                false
+                              end
+      end
+
+      option_white_list = cli_option_value_raw OPTION_WHITE_LIST, SEPARATOR, log
+
+      [
+        option_github_stars_only,
+        option_head,
+        option_pull_request,
+        option_white_list
+      ]
+    end
+
+    def cli_process_misc(argv1, log)
+      argv1_is_http = argv1.match(/^http/)
+
+      unless argv1_is_http
+        begin
+          found_file_content = File.read(argv1)
+        rescue StandardError => e
+          log.verbose "Not a file: #{e.to_s.red}"
+        end
+      end
+
       number_of_threads = cli_option_value OPTION_THREADS, SEPARATOR, log
       number_of_threads = DEFAULT_NUMBER_OF_THREADS if number_of_threads.nil?
-      option_white_list = cli_option_value_raw OPTION_WHITE_LIST, SEPARATOR, log
+
+      [
+        argv1_is_http,
+        found_file_content,
+        number_of_threads
+      ]
+    end
+
+    def cli_process(argv1, argv_flags, log)
+      argv1_is_http,
+      found_file_content,
+      number_of_threads = cli_process_misc(argv1, log)
+
+      option_github_stars_only,
+      option_head,
+      option_pull_request,
+      option_white_list = cli_process_options(argv1, argv1_is_http, log)
+
+      flag_fetch_github_stars,
+      flag_minimize_output,
+      flag_control_failure = cli_process_flags(option_github_stars_only,
+                                               argv_flags)
 
       [
         option_github_stars_only,
