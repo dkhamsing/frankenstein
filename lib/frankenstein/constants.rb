2028a3658e6cc6e4de3bef5ead280356fd3f2631
dkhamsing <dkhamsing8@gmail.com>
2015-11-13 19:39:42 -0800

    update constants

--- lib/frankenstein/constants.rb
+++ lib/frankenstein/constants.rb
@@ -100,6 +100,7 @@ module Frankenstein
     '://instagram.com/',
     'paypal.com/cgi-bin/webscr',
     'plus.google.com/share',
+    'readthedocs.org/',
     'reddit.com/message/compose',
     '://secure.travis-ci.org/.*(pn|sv)g',
     '://stackoverflow.com/questions/ask?',
