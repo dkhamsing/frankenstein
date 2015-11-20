e8d4034873c6a4ef535c4ea8224195596f43f725
dkhamsing <dkhamsing@cirruspath.com>
2015-11-20 09:15:09 -0800

    automate new

--- lib/frankenstein/new.rb
+++ lib/frankenstein/new.rb
@@ -31,7 +31,13 @@ module New
 
   puts '> Getting notifications'
   n = client.notifications
-  # pp n
+
+  t = Time.new.strftime('%b %d at %I:%M%p')
+
+  if n.count == 0
+    puts "#{'No notifications'.green} #{t.white}"
+    exit
+  end
 
   m = n.map do |x|
     s = x[:subject]
@@ -40,73 +46,46 @@ module New
     # is it better to load `u` and retrieve html_url via json ?
   end
 
-  t = Time.new.strftime('%b %d at %I:%M%p')
-  puts "#{'No notifications'.green} #{t.white}" if n.count == 0
-
-  while m.count > 0
-    m.each_with_index do |x, index|
-      print "#{index + 1}  "
-      if x.include? RUN_ISSUES
-        notif = n[index]
-        subject = notif['subject']['title']
-        print x.yellow + ' '
-        puts subject
-      else
-        puts x.blue
-      end
-    end
-
-    print '> Enter number to process (or enter to exit): '
-    user_input = STDIN.gets.chomp
-    puts user_input
-
-    if user_input.number?
-      index = user_input.to_i - 1
-
-      url = m[index]
-      m.delete_at(index)
-      puts url
-
-      unless url.include? RUN_ISSUES
-        Frankenstein.core_merge url
-      else
-        notif = n[index]
-
-        r = notif['repository']['name']
-        user = Frankenstein.github_netrc_username
-        repo = "#{user}/#{r}"
-
-        j = notif['subject']['url']
-
-        links_to_check, json = Frankenstein.core_links_to_check repo, j
-
-        number = json['number']
-        links_to_check.each do |x|
-          item = {
-            repo: x,
-            # might not need issue hash
-            issue: {
-              repo: repo,
-              number: number
-            }
-          }
-
-          Frankenstein.core_todo_add item
-          puts "Added #{x.white} to #{'todo'.blue}"
-
-          client = Frankenstein.github_client
-
-          comment = "Run request for #{x} received."
-          client.add_comment repo, number, comment
-
-          thread = notif['id']
-          client.mark_thread_as_read thread
-        end # end links_to_check..
-      end
+  m.each_with_index do |x, index|
+    print "#{index + 1} "
+    if x.include? RUN_ISSUES
+      notif = n[index]
+      subject = notif['subject']['title']
+      print x.yellow + ' '
+      puts subject
     else
-      exit
+      puts x.blue
     end
-  end # while
+
+    sleep 0.5
+
+    url = m[index]
+    if url.include? RUN_ISSUES
+      notif = n[index]
+
+      r = notif['repository']['name']
+      user = Frankenstein.github_netrc_username
+      repo = "#{user}/#{r}"
+
+      j = notif['subject']['url']
+
+      links_to_check, json = Frankenstein.core_links_to_check repo, j
+
+      number = json['number']
+      links_to_check.each do |x|
+        Frankenstein.core_todo_add({ repo: x })
+        puts "Added #{x.white} to #{'todo'.blue}"
+
+        comment = "Run request for #{x} received."
+        client.add_comment repo, number, comment
+
+        thread = notif['id']
+        client.mark_thread_as_read thread
+      end # end links_to_check..
+    else
+      Frankenstein.core_merge url
+    end # if url.include? RUN_ISSUES
+  end # end m.each
 
   puts "\n#{PRODUCT.white} finished " if n.count > 0
 end # module
